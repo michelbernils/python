323a384a37c331658ab9c29a8c8df2c228cc30c2
Michel Krahenbuhl Bernils <bernils.michel@gmail.com>
2018-10-24 01:58:02 -0300

    Mais,Mais e Mais funções prints

--- print.py
+++ print.py
@@ -3,6 +3,22 @@ subst = "Python"
 verbo = "é"
 adjetivo = "fantástico"
 print(subst, verbo, adjetivo, sep="_", end="!\n")
+
+name = "matheus ferreira de carvalho"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("Hello, " + name.title() + "!")
+print("\tHello")
+print(name.rsplit())
+
+favorite_language = ' python '
+print(favorite_language)
+print(favorite_language.lstrip())
+print(favorite_language.rsplit())
+print(favorite_language.split())
+
 dia = 15
 mes = 10
 ano = 2015
