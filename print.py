dc37385445fa09bd76fe3e27c2d86bd29fb29ecd
Michel Krahenbuhl Bernils <bernils.michel@gmail.com>
2018-10-24 00:55:28 -0300

    Famoso, Hello World

---
+++ print.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
+dia = 15
+mes = 10
+ano = 2015
+print(dia, mes, ano, sep="/", end="\n")
