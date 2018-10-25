0a5024b99c3c30080fca7585cf4d7f2116b86ed0
Michel Krahenbuhl Bernils <bernils.michel@gmail.com>
2018-10-24 22:04:55 -0300

    tuple start

---
+++ tuple.py
@@ -0,0 +1,11 @@
+dimensions = (200, 50)
+
+print("Original Dimension: ")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 50)
+
+print("\nModified Dimension: ")
+for dimension in dimensions:
+    print(dimension)
