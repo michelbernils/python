c04e5ec60187c1042054c1fa4711fb22aac2be56
Michel Krahenbuhl Bernils <bernils.michel@gmail.com>
2018-10-28 21:46:33 -0300

    ship game of python crash course

---
+++ ship_game/settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """ A class that store all settings for Alien Invasion """
+
+    def __init__(self):
+        #Screen Settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        
